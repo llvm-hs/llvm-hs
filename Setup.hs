7195b31a2e58430a79f87f60e45ccbc773c4b1d1
Benjamin S. Scarlet <fgthb0@greynode.net>
2013-06-25 14:33:55 -0700

    Merge pull request #38 from Ralith/extern-libs
    Fix library ordering

--- Setup.hs
+++ Setup.hs
@@ -73,8 +73,8 @@ main = do
                 condTreeComponents = condTreeComponents libraryCondTree ++ [
                   (
                     Var (Flag (FlagName "shared-llvm")),
-                    CondNode (mempty { libBuildInfo = mempty { extraLibs = externLibs ++ [sharedLib] } }) [] [],
-                    Just (CondNode (mempty { libBuildInfo = mempty { extraLibs = externLibs ++ staticLibs } }) [] [])
+                    CondNode (mempty { libBuildInfo = mempty { extraLibs = [sharedLib] ++ externLibs } }) [] [],
+                    Just (CondNode (mempty { libBuildInfo = mempty { extraLibs = staticLibs ++ externLibs } }) [] [])
                   )
                 ] 
               }
