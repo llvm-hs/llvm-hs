153613bbe23eddfa90146a73bd955f8649dd5942
Benjamin S. Scarlet <scarlet@alum.mit.edu>
2013-07-04 13:37:29 -0400

    Doc.

--- src/LLVM/General/Internal/Module.hs
+++ src/LLVM/General/Internal/Module.hs
@@ -90,9 +90,11 @@ emitToFile fileType (TargetMachine tm) path (Module m) = flip runAnyContT return
   result <- decodeM =<< (liftIO $ FFI.targetMachineEmitToFile tm m path fileType msgPtr)
   when result $ fail =<< decodeM =<< anyContToM (bracket (peek msgPtr) free)
 
+-- | write target-specific assembly directly into a file
 writeAssemblyToFile :: TargetMachine -> FilePath -> Module -> IO ()
 writeAssemblyToFile = emitToFile FFI.codeGenFileTypeAssembly
 
+-- | write target-specific object code directly into a file
 writeObjectToFile :: TargetMachine -> FilePath -> Module -> IO ()
 writeObjectToFile = emitToFile FFI.codeGenFileTypeObject
 
@@ -107,9 +109,11 @@ emitToByteString fileType (TargetMachine tm) (Module m) = flip runAnyContT retur
     else
         decodeM =<< anyContToM (bracket (peek memoryBufferPtr) FFI.disposeMemoryBuffer)
 
+-- | produce target-specific assembly as a 'String'
 moduleAssembly :: TargetMachine -> Module -> IO String
 moduleAssembly tm m = decodeM . UTF8ByteString =<< emitToByteString FFI.codeGenFileTypeAssembly tm m
 
+-- | produce target-specific object code as a 'ByteString'
 moduleObject :: TargetMachine -> Module -> IO ByteString
 moduleObject = emitToByteString FFI.codeGenFileTypeObject
 
--- src/LLVM/General/Module.hs
+++ src/LLVM/General/Module.hs
@@ -1,5 +1,5 @@
 -- | A 'Module' holds a C++ LLVM IR module. 'Module's may be converted to or from strings or Haskell ASTs, or
--- added to an 'LLVM.General.ExecutionEngine' and so JIT compiled to get function pointers.
+-- added to an 'LLVM.General.ExecutionEngine.ExecutionEngine' and so JIT compiled to get function pointers.
 module LLVM.General.Module (
     Module,
     withModuleFromAST,
