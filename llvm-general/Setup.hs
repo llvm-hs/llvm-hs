5582229d68bed5726e04da5be5a84785281ed5bf
Benjamin S. Scarlet <scarlet@alum.mit.edu>
2015-04-04 20:47:08 -0400

    update Setup.hs for new llvm-config interface

--- llvm-general/Setup.hs
+++ llvm-general/Setup.hs
@@ -111,7 +111,7 @@ main = do
                         "3.2" -> "LLVM-3.2svn"
                         x -> "LLVM-" ++ x
       staticLibs <- liftM (map (fromJust . stripPrefix "-l") . words) $ llvmConfig ["--libs"]
-      externLibs <- liftM ((++ ["tinfo"]) . mapMaybe (stripPrefix "-l") . words) $ llvmConfig ["--ldflags"]
+      systemLibs <- liftM (map (fromJust . stripPrefix "-l") . words) $ llvmConfig ["--system-libs"]
 
       let genericPackageDescription' = genericPackageDescription {
             condLibrary = do
@@ -123,8 +123,8 @@ main = do
                 condTreeComponents = condTreeComponents libraryCondTree ++ [
                   (
                     Var (Flag (FlagName "shared-llvm")),
-                    CondNode (mempty { libBuildInfo = mempty { extraLibs = [sharedLib] ++ externLibs } }) [] [],
-                    Just (CondNode (mempty { libBuildInfo = mempty { extraLibs = staticLibs ++ externLibs } }) [] [])
+                    CondNode (mempty { libBuildInfo = mempty { extraLibs = [sharedLib] ++ systemLibs } }) [] [],
+                    Just (CondNode (mempty { libBuildInfo = mempty { extraLibs = staticLibs ++ systemLibs } }) [] [])
                   )
                 ] 
               }
