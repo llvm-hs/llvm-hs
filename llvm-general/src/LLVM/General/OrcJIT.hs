9aa5c1687824790d07babb816824ac132d1f6e4b
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-08-31 16:17:29 +0200

    Fix formatting of exports in LLVM.General.OrcJIT

--- llvm-general/src/LLVM/General/OrcJIT.hs
+++ llvm-general/src/LLVM/General/OrcJIT.hs
@@ -1,17 +1,17 @@
-module LLVM.General.OrcJIT
-  (IRCompileLayer,
-  JITSymbol(..),
-  JITSymbolFlags(..),
-  MangledSymbol,
-  ModuleSet,
-  ObjectLinkingLayer,
-  SymbolResolver(..),
-  SymbolResolverFn,
-  findSymbol,
-  mangleSymbol,
-  withIRCompileLayer,
-  withModuleSet,
-  withObjectLinkingLayer)
-  where
+module LLVM.General.OrcJIT (
+    IRCompileLayer,
+    JITSymbol(..),
+    JITSymbolFlags(..),
+    MangledSymbol,
+    ModuleSet,
+    ObjectLinkingLayer,
+    SymbolResolver(..),
+    SymbolResolverFn,
+    findSymbol,
+    mangleSymbol,
+    withIRCompileLayer,
+    withModuleSet,
+    withObjectLinkingLayer
+  ) where
 
 import           LLVM.General.Internal.OrcJIT
