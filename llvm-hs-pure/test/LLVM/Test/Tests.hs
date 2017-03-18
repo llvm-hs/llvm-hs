f29f60b61a4adfb074c30a80170a42260cff85a4
Daniel Mendler <mail@daniel-mendler.de>
2017-03-18 14:34:15 +0100

    llvm-hs-pure: remove prettyprint

--- llvm-hs-pure/test/LLVM/Test/Tests.hs
+++ llvm-hs-pure/test/LLVM/Test/Tests.hs
@@ -3,9 +3,7 @@ module LLVM.Test.Tests where
 import Test.Framework
 
 import qualified LLVM.Test.DataLayout as DataLayout
-import qualified LLVM.Test.PrettyPrint as PrettyPrint
 
 tests = testGroup "llvm-hs" [
-    DataLayout.tests,
-    PrettyPrint.tests
+    DataLayout.tests
   ]
