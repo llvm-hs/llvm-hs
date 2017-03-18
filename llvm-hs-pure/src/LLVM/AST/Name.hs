31cd28cdc680dbc8322754053f446f15e1453041
Daniel Mendler <mail@daniel-mendler.de>
2017-03-18 11:20:26 +0100

    add IsString Name (#49)

--- llvm-hs-pure/src/LLVM/AST/Name.hs
+++ llvm-hs-pure/src/LLVM/AST/Name.hs
@@ -2,12 +2,13 @@
 module LLVM.AST.Name where
 
 import LLVM.Prelude
+import Data.String
 
 {- |
 Objects of various sorts in LLVM IR are identified by address in the LLVM C++ API, and
 may be given a string name. When printed to (resp. read from) human-readable LLVM assembly, objects without
 string names are numbered sequentially (resp. must be numbered sequentially). String names may be quoted, and
-are quoted when printed if they would otherwise be misread - e.g. when containing special characters. 
+are quoted when printed if they would otherwise be misread - e.g. when containing special characters.
 
 > 7
 
@@ -24,8 +25,11 @@ printed as assembly or translated into a Haskell AST, unnamed nodes will be renu
 unnamed node numbers should be thought of as having any scope limited to the 'LLVM.AST.Module' in
 which they are used.
 -}
-data Name 
-    = Name String -- ^ a string name 
+data Name
+    = Name String -- ^ a string name
     | UnName Word -- ^ a number for a nameless thing
    deriving (Eq, Ord, Read, Show, Typeable, Data)
+
+instance IsString Name where
+  fromString = Name
 
