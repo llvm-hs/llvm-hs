e19c9946c3b10c771507619ee757cba3932e1219
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-10-10 06:50:01 +0200

    Support more calling conventions

--- llvm-hs/src/LLVM/Internal/CallingConvention.hs
+++ llvm-hs/src/LLVM/Internal/CallingConvention.hs
@@ -21,12 +21,14 @@ instance Monad m => EncodeM m A.CC.CallingConvention FFI.CallingConvention where
           A.CC.C -> FFI.callingConventionC
           A.CC.Fast -> FFI.callingConventionFast
           A.CC.Cold -> FFI.callingConventionCold
-          A.CC.GHC ->  FFI.callingConventionGHC
+          A.CC.GHC -> FFI.callingConventionGHC
           A.CC.HiPE -> FFI.callingConventionHiPE
           A.CC.WebKit_JS -> FFI.callingConventionWebKit_JS
           A.CC.AnyReg -> FFI.callingConventionAnyReg
           A.CC.PreserveMost -> FFI.callingConventionPreserveMost
           A.CC.PreserveAll -> FFI.callingConventionPreserveAll
+          A.CC.Swift -> FFI.callingConventionSwift
+          A.CC.CXX_FastTLS -> FFI.callingConventionCXX_FAST_TLS
           A.CC.X86_StdCall -> FFI.callingConventionX86_StdCall
           A.CC.X86_FastCall -> FFI.callingConventionX86_FastCall
           A.CC.ARM_APCS -> FFI.callingConventionARM_APCS
@@ -41,6 +43,21 @@ instance Monad m => EncodeM m A.CC.CallingConvention FFI.CallingConvention where
           A.CC.Intel_OCL_BI -> FFI.callingConventionIntel_OCL_BI
           A.CC.X86_64_SysV -> FFI.callingConventionX86_64_SysV
           A.CC.Win64 -> FFI.callingConventionWin64
+          A.CC.X86_VectorCall -> FFI.callingConventionX86_VectorCall
+          A.CC.HHVM -> FFI.callingConventionHHVM
+          A.CC.HHVM_C -> FFI.callingConventionHHVM_C
+          A.CC.X86_Intr -> FFI.callingConventionX86_INTR
+          A.CC.AVR_Intr -> FFI.callingConventionAVR_INTR
+          A.CC.AVR_Signal -> FFI.callingConventionAVR_SIGNAL
+          A.CC.AVR_Builtin -> FFI.callingConventionAVR_BUILTIN
+          A.CC.AMDGPU_VS -> FFI.callingConventionAMDGPU_VS
+          A.CC.AMDGPU_HS -> FFI.callingConventionAMDGPU_HS
+          A.CC.AMDGPU_GS -> FFI.callingConventionAMDGPU_GS
+          A.CC.AMDGPU_PS -> FFI.callingConventionAMDGPU_PS
+          A.CC.AMDGPU_CS -> FFI.callingConventionAMDGPU_CS
+          A.CC.AMDGPU_Kernel -> FFI.callingConventionAMDGPU_KERNEL
+          A.CC.X86_RegCall -> FFI.callingConventionX86_RegCall
+          A.CC.MSP430_Builtin -> FFI.callingConventionMSP430_BUILTIN
           A.CC.Numbered cc' -> FFI.CallingConvention (fromIntegral cc')
 
 instance Monad m => DecodeM m A.CC.CallingConvention FFI.CallingConvention where
@@ -54,6 +71,8 @@ instance Monad m => DecodeM m A.CC.CallingConvention FFI.CallingConvention where
     [callingConventionP|AnyReg|] -> A.CC.AnyReg
     [callingConventionP|PreserveMost|] -> A.CC.PreserveMost
     [callingConventionP|PreserveAll|] -> A.CC.PreserveAll
+    [callingConventionP|Swift|] -> A.CC.Swift
+    [callingConventionP|CXX_FAST_TLS|] -> A.CC.CXX_FastTLS
     [callingConventionP|X86_StdCall|] -> A.CC.X86_StdCall
     [callingConventionP|X86_FastCall|] -> A.CC.X86_FastCall
     [callingConventionP|ARM_APCS|] -> A.CC.ARM_APCS
@@ -68,6 +87,21 @@ instance Monad m => DecodeM m A.CC.CallingConvention FFI.CallingConvention where
     [callingConventionP|Intel_OCL_BI|] -> A.CC.Intel_OCL_BI
     [callingConventionP|X86_64_SysV|] -> A.CC.X86_64_SysV
     [callingConventionP|Win64|] -> A.CC.Win64
+    [callingConventionP|X86_VectorCall|] -> A.CC.X86_VectorCall
+    [callingConventionP|HHVM|] -> A.CC.HHVM
+    [callingConventionP|HHVM_C|] -> A.CC.HHVM_C
+    [callingConventionP|X86_INTR|] -> A.CC.X86_Intr
+    [callingConventionP|AVR_INTR|] -> A.CC.AVR_Intr
+    [callingConventionP|AVR_SIGNAL|] -> A.CC.AVR_Signal
+    [callingConventionP|AVR_BUILTIN|] -> A.CC.AVR_Builtin
+    [callingConventionP|AMDGPU_VS|] -> A.CC.AMDGPU_VS
+    [callingConventionP|AMDGPU_HS|] -> A.CC.AMDGPU_HS
+    [callingConventionP|AMDGPU_GS|] -> A.CC.AMDGPU_GS
+    [callingConventionP|AMDGPU_PS|] -> A.CC.AMDGPU_PS
+    [callingConventionP|AMDGPU_CS|] -> A.CC.AMDGPU_CS
+    [callingConventionP|AMDGPU_KERNEL|] -> A.CC.AMDGPU_Kernel
+    [callingConventionP|X86_RegCall|] -> A.CC.X86_RegCall
+    [callingConventionP|MSP430_BUILTIN|] -> A.CC.MSP430_Builtin
     FFI.CallingConvention (CUInt ci)
       | ci >= 64 -> A.CC.Numbered (fromIntegral ci)
       | otherwise -> error ("Unknown calling convention: " <> show ci)
--- llvm-hs/src/LLVM/Internal/FFI/CallingConvention.h
+++ llvm-hs/src/LLVM/Internal/FFI/CallingConvention.h
@@ -2,29 +2,46 @@
 #define __LLVM_INTERNAL_FFI__CALLING_CONVENTION__H__
 
 #define LLVM_HS_FOR_EACH_CALLING_CONVENTION(macro) \
-  macro(C, 0)                                           \
-  macro(Fast, 8)                                        \
-  macro(Cold, 9)                                        \
-  macro(GHC, 10)                                        \
-  macro(HiPE, 11)                                       \
-  macro(WebKit_JS, 12)                                  \
-  macro(AnyReg, 13)                                     \
-  macro(PreserveMost, 14)                               \
-  macro(PreserveAll, 15)                                \
-  macro(X86_StdCall, 64)                                \
-  macro(X86_FastCall, 65)                               \
-  macro(ARM_APCS, 66)                                   \
-  macro(ARM_AAPCS, 67)                                  \
-  macro(ARM_AAPCS_VFP, 68)                              \
-  macro(MSP430_INTR, 69)                                \
-  macro(X86_ThisCall, 70)                               \
-  macro(PTX_Kernel, 71)                                 \
-  macro(PTX_Device, 72)                                 \
-  macro(SPIR_FUNC, 75)                                  \
-  macro(SPIR_KERNEL, 76)                                \
-  macro(Intel_OCL_BI, 77)                               \
-  macro(X86_64_SysV, 78)                                \
-  macro(Win64, 79)
+  macro(C, 0)                                      \
+  macro(Fast, 8)                                   \
+  macro(Cold, 9)                                   \
+  macro(GHC, 10)                                   \
+  macro(HiPE, 11)                                  \
+  macro(WebKit_JS, 12)                             \
+  macro(AnyReg, 13)                                \
+  macro(PreserveMost, 14)                          \
+  macro(PreserveAll, 15)                           \
+  macro(Swift, 16)                                 \
+  macro(CXX_FAST_TLS, 17)                          \
+  macro(X86_StdCall, 64)                           \
+  macro(X86_FastCall, 65)                          \
+  macro(ARM_APCS, 66)                              \
+  macro(ARM_AAPCS, 67)                             \
+  macro(ARM_AAPCS_VFP, 68)                         \
+  macro(MSP430_INTR, 69)                           \
+  macro(X86_ThisCall, 70)                          \
+  macro(PTX_Kernel, 71)                            \
+  macro(PTX_Device, 72)                            \
+  macro(SPIR_FUNC, 75)                             \
+  macro(SPIR_KERNEL, 76)                           \
+  macro(Intel_OCL_BI, 77)                          \
+  macro(X86_64_SysV, 78)                           \
+  macro(Win64, 79)                                 \
+  macro(X86_VectorCall, 80)                        \
+  macro(HHVM, 81)                                  \
+  macro(HHVM_C, 82)                                \
+  macro(X86_INTR, 83)                              \
+  macro(AVR_INTR, 84)                              \
+  macro(AVR_SIGNAL, 85)                            \
+  macro(AVR_BUILTIN, 86)                           \
+  macro(AMDGPU_VS, 87)                             \
+  macro(AMDGPU_GS, 88)                             \
+  macro(AMDGPU_PS, 89)                             \
+  macro(AMDGPU_CS, 90)                             \
+  macro(AMDGPU_KERNEL, 91)                         \
+  macro(X86_RegCall, 92)                           \
+  macro(AMDGPU_HS, 93)                             \
+  macro(MSP430_BUILTIN, 94)
 
 typedef enum {
 #define ENUM_CASE(l,n) LLVM_Hs_CallingConvention_ ## l = n,
