ffa7b60aa6a350e0d76a3afacd9c7aa11416f241
Stephen Diehl <stephen.m.diehl@gmail.com>
2017-11-26 11:56:27 +0000

    Added simple example for REAMDE

--- src/IRBuilder.hs
+++ src/IRBuilder.hs
@@ -29,6 +29,18 @@ c1 = ConstantOperand $ C.Float (F.Double 10)
 c2 :: Operand
 c2 = ConstantOperand $ C.Int 32 10
 
+simple :: IO ()
+simple = T.putStrLn $ ppllvm $ mkModule $ execModuleBuilder emptyModuleBuilder $ mdo
+
+  function "add" [(i32, Just "a"), (i32, (Just "b"))] i32 $ \[a,b] -> mdo
+
+    entry <- block `named` "entry"; do
+      c <- add a b
+      ret c
+
+  where
+    mkModule ds = defaultModule { moduleName = "exampleModule", moduleDefinitions = ds }
+
 example :: IO ()
 example = T.putStrLn $ ppllvm $ mkModule $ execModuleBuilder emptyModuleBuilder $ mdo
 
