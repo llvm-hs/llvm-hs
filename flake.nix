59e39e249bf2636cd252fbb960b88467260305e1
Providence Salumu <Providence.Salumu@smunix.com>
2021-12-30 22:46:51 -0500

    nix: add flake to llvm-12

--- flake.nix
+++ flake.nix
@@ -33,11 +33,11 @@
               llvm-hs-pure = overrideCabal (callCabal2nix "llvm-hs-pure"
                 (with nf.lib; filter { root = ./llvm-hs-pure; }) { })
                 (o: { version = "${o.version}-${version ghc}"; });
-              llvm-hs = overrideCabal (callCabal2nix "llvm-hs"
+              llvm-hs = addBuildDepends (overrideCabal (callCabal2nix "llvm-hs"
                 (with nf.lib; filter { root = ./llvm-hs; }) {
                   inherit llvm-hs-pure;
-                  llvm-config = llvmPackages_9.llvm;
-                }) (o: { version = "${o.version}-${version ghc}"; });
+                }) (o: { version = "${o.version}-${version ghc}"; }))
+                [ llvmPackages_12.llvm ];
               llvm-hs-pretty = dontCheck (overrideCabal (addBuildTools
                 (callCabal2nix "llvm-hs-pretty" "${lhp}" {
                   inherit llvm-hs llvm-hs-pure;
@@ -78,7 +78,7 @@
                   cabal-install
                   ghc
                   haskell-language-server
-                  llvmPackages_9.llvm
+                  llvmPackages_12.llvm
                 ];
               };
           };
