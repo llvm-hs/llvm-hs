789e2b9e8b827d4c5e15b997f16e395fd2259c0f
Adam Paszke <apaszke@google.com>
2022-03-16 11:17:07 +0000

    Add INLINE annotations to monadic typeclasses
    Those are generally tiny functions that we should encourage GHC to
    inline. The same approach is taken e.g. by the transformers library.

--- llvm-hs/src/Control/Monad/AnyCont/Class.hs
+++ llvm-hs/src/Control/Monad/AnyCont/Class.hs
@@ -22,30 +22,37 @@ class MonadAnyCont b m where
 
 instance MonadTransAnyCont b m => MonadAnyCont b (AnyContT m) where
   anyContToM c = AnyCont.anyContT (liftAnyCont c)
+  {-# INLINE anyContToM #-}
 
 instance Monad m => ScopeAnyCont (AnyContT m) where
   scopeAnyCont = lift . AnyCont.runAnyContT' return
+  {-# INLINE scopeAnyCont #-}
 
 
 instance (Monad m, MonadAnyCont b m) => MonadAnyCont b (StateT s m) where
   anyContToM x = lift $ anyContToM x
+  {-# INLINE anyContToM #-}
 
 instance ScopeAnyCont m => ScopeAnyCont (StateT s m) where
   scopeAnyCont = StateT . (scopeAnyCont .) . runStateT
+  {-# INLINE scopeAnyCont #-}
 
 
 instance (Monad m, MonadAnyCont b m) => MonadAnyCont b (ExceptT e m) where
   anyContToM x = lift $ anyContToM x
-
+  {-# INLINE anyContToM #-}
 
 instance ScopeAnyCont m => ScopeAnyCont (ExceptT e m) where
   scopeAnyCont = mapExceptT scopeAnyCont
+  {-# INLINE scopeAnyCont #-}
+
 
 class MonadTransAnyCont b m where
   liftAnyCont :: (forall r . (a -> b r) -> b r) -> (forall r . (a -> m r) -> m r)
 
 instance MonadTransAnyCont b b where
   liftAnyCont c = c
+  {-# INLINE liftAnyCont #-}
 
 instance MonadTransAnyCont b m => MonadTransAnyCont b (StateT s m) where
   liftAnyCont c = (\c q -> StateT $ \s -> c $ ($ s) . runStateT . q) (liftAnyCont c)
