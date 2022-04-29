f8b006c22c305bf0a6d61669d6a3490e80bd4bba
Adam Paszke <apaszke@google.com>
2022-04-29 16:11:53 +0000

    Add support for custom pass pipeline
    We only support a few non-default module passes for now, but it lays
    down the infrastructure for other passes.

--- llvm-hs/src/LLVM/Internal/FFI/Passes.hs
+++ llvm-hs/src/LLVM/Internal/FFI/Passes.hs
@@ -5,6 +5,7 @@ module LLVM.Internal.FFI.Passes where
 import LLVM.Prelude
 
 import Foreign.Ptr
+import Foreign.C.Types
 import Foreign.C.String
 
 import LLVM.Internal.FFI.Error
@@ -20,3 +21,33 @@ foreign import ccall unsafe "LLVMDisposePassBuilderOptions" disposePassBuilderOp
 foreign import ccall unsafe "LLVMRunPasses" runPasses
   :: Ptr Module -> CString -> Ptr TargetMachine -> Ptr PassBuilderOptions -> IO (Ptr Error)
 
+data PassBuilderPackage
+data ModulePassManager
+data ModulePass
+
+foreign import ccall unsafe "LLVM_Hs_CreatePassBuilderPackage" createPassBuilderPackage
+  :: Ptr TargetMachine -> IO (Ptr PassBuilderPackage)
+
+foreign import ccall unsafe "LLVM_Hs_DisposePassBuilderPackage" disposePassBuilderPackage
+  :: Ptr PassBuilderPackage -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_CreateModulePassManager" createModulePassManager
+  :: IO (Ptr ModulePassManager)
+
+foreign import ccall unsafe "LLVM_Hs_DisposeModulePassManager" disposeModulePassManager
+  :: Ptr ModulePassManager -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_ModulePassManagerRun" modulePassManagerRun
+  :: Ptr ModulePassManager -> Ptr PassBuilderPackage -> Ptr Module -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_AddPerModuleDefaultPipeline" addPerModuleDefaultPipeline
+  :: Ptr ModulePassManager -> Ptr PassBuilderPackage -> CInt -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_AddGlobalDeadCodeEliminationPass" addGlobalDeadCodeEliminationPass
+  :: Ptr ModulePassManager -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_AddAlwaysInlinePass" addAlwaysInlinePass
+  :: Ptr ModulePassManager -> CInt -> IO ()
+
+foreign import ccall unsafe "LLVM_Hs_AddInternalizeFunctionsPass" addInternalizeFunctionsPass
+  :: Ptr ModulePassManager -> CInt -> Ptr CString -> IO ()
--- llvm-hs/src/LLVM/Passes.hs
+++ llvm-hs/src/LLVM/Passes.hs
@@ -1,4 +1,4 @@
 -- | This module provides an interface to LLVM's passes.
-module LLVM.Passes (PassSetSpec (..), runPasses) where
+module LLVM.Passes (PassSetSpec (..), ModulePass (..), runPasses) where
 
 import LLVM.Internal.Passes
